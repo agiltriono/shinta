996b1a943052ae56180f1a95c41c48d2cd2ab415
agiltriono <agiltriono71@yahoo.com>
2023-06-11 09:41:26 +0700

    FIX linkremover channel return undefined value

--- util/linkremover.js
+++ util/linkremover.js
@@ -1,16 +1,15 @@
 module.exports = async function linkremover(msg, voice){
   const channelId = msg.channelId
   const vc = voice
-  const game = vc.game[channelId]
-  const temp = vc.temp[channelId]
-  const vclink = vc.allow_link
+  const game = vc.game[channelId];
+  const temp = vc.temp[channelId];
   const content = msg.content.toLowerCase()
-  if (temp && vclink) {
-    const filter = vclink.trim().split(",");
+  if (temp) {
+    const filter = vc.allow_link.trim().split(",");
     if(filter.length != 0 && !filter.some(c => content.includes(c))) return await msg.delete();
     return;
-  } else if (game && vclink) {
-    const filter = vclink.trim().split(",");
+  } else if (game) {
+    const filter = vc.allow_link.trim().split(",");
     if(filter.length != 0 && !filter.some(c => content.includes(c))) return await msg.delete();
     return;
   } else {
